b7bfb0648c7bd96b51193c2eadcc02e36c3af174
james <james@bubbleye.com>
2022-08-15 22:20:33 +0800

    Move functions to separate areas

---
+++ dbcon/queries.py
@@ -0,0 +1,116 @@
+import pandas as pd
+from .connection import PostgresCon
+from config import get_logger
+import numpy as np
+
+logger = get_logger(__name__)
+
+
+def upsert_df(
+    table_name,
+    insert_columns,
+    df,
+    key_columns,
+    database_connection: PostgresCon,
+    log=None,
+):
+    db_cols_str = ", ".join([f'"{col}"' for col in insert_columns])
+    key_cols_str = ", ".join([f'"{col}"' for col in key_columns])
+    values_str = ", ".join([f"%({col})s" for col in insert_columns])
+    set_update = ", ".join([f"{col} = excluded.{col}" for col in insert_columns])
+    if isinstance(df, pd.Series):
+        df = pd.DataFrame(df).T
+    for col in insert_columns:
+        if pd.api.types.is_string_dtype(df[col]):
+            df[col] = df[col].apply(lambda x: x.replace("'", "''"))
+    insert_query = f""" 
+        INSERT INTO {table_name} ({db_cols_str})
+        VALUES ({values_str})
+        ON CONFLICT ({key_cols_str})
+        DO UPDATE SET {set_update}
+        """
+    values = df[insert_columns].to_dict("records")
+    if log:
+        logger.info(f"MY INSERT QUERY: {insert_query.format(values)}")
+    with database_connection.engine.begin() as connection:
+        connection.execute(insert_query, values)
+
+
+def insert_get(
+    table_name: str,
+    df: pd.DataFrame | pd.Series,
+    insert_columns: str | list[str],
+    key_columns: str | list[str],
+    database_connection: PostgresCon,
+    log: bool | None = None,
+) -> pd.DataFrame:
+    logger.info(f"insert_get table: {table_name}")
+
+    if isinstance(insert_columns, str):
+        insert_columns = [insert_columns]
+    if isinstance(key_columns, str):
+        key_columns = [key_columns]
+    if isinstance(df, pd.Series):
+        df = pd.DataFrame(df).T
+    upsert_df(
+        table_name,
+        insert_columns,
+        df,
+        key_columns,
+        database_connection=database_connection,
+    )
+    get_df = query_all(
+        table_name, key_columns, df, database_connection=database_connection
+    )
+    return get_df
+
+
+def query_pub_domains(database_connection):
+    # Query Pub Domain Table
+    sel_query = """SELECT id, url, crawled_at
+        FROM pub_domains
+        ORDER BY crawled_at NULLS FIRST
+        limit 10000
+        """
+    df = pd.read_sql(sel_query, database_connection.engine)
+    return df
+
+
+def query_store_apps(stores, database_connection):
+    where_str = "store IN (" + (", ").join([str(x) for x in stores]) + ")"
+    if stores[0] == 1:
+        where_str += " AND installs >= 100000"
+    sel_query = f"""SELECT store, id as store_app, store_id, updated_at  
+        FROM store_apps
+        WHERE {where_str}
+        ORDER BY updated_at NULLS FIRST
+        limit 1000
+        """
+    df = pd.read_sql(sel_query, database_connection.engine)
+    return df
+
+
+def query_all(
+    table_name: str, key_cols: list[str] | str, df: pd.DataFrame, database_connection
+) -> pd.DataFrame:
+    if isinstance(key_cols, str):
+        key_cols = [key_cols]
+    wheres = []
+    for key_col in key_cols:
+        keys = df[key_col].unique().tolist()
+        if all([isinstance(x, (np.integer, int)) for x in keys]):
+            values_str = "(" + (", ").join([str(x) for x in keys]) + ")"
+            values_str = values_str.replace("%", "%%")
+        else:
+            values_str = "('" + ("', '").join(keys) + "')"
+            values_str = values_str.replace("%", "%%")
+        where = f"{key_col} IN {values_str}"
+        wheres.append(where)
+    where_str = " AND ".join(wheres)
+    sel_query = f"""SELECT *
+    FROM {table_name}
+    WHERE {where_str}
+    """
+    # logger.info(sel_query)
+    df = pd.read_sql(sel_query, database_connection.engine)
+    return df
---
+++ tools/from_csv.py
@@ -0,0 +1,52 @@
+from config import MODULE_DIR, get_logger
+import pandas as pd
+from dbcon.queries import upsert_df
+
+logger = get_logger(__name__)
+
+
+def reinsert_from_csv():
+
+    filename = f"{MODULE_DIR}/store-data/763K_plus_IOS_Apps_Info.csv"
+    chunksize = 10000
+    i = 0
+    store = 2
+    platform = 2
+
+    with pd.read_csv(filename, chunksize=chunksize) as reader:
+        for chunk in reader:
+            logger.info(f"chunk {i}")
+            chunk["platform"] = platform
+            chunk["store"] = store
+            chunk.columns = [x.replace(" ", "_").lower() for x in chunk.columns]
+            if store == 1:
+                insert_columns = chunk.columns.tolist()
+            if store == 2:
+                chunk = chunk.rename(
+                    columns={
+                        "ios_app_id": "app_id",
+                        "title": "app_name",
+                        "developer_ios_id": "developer_id",
+                        "current_version_release_date": "last_updated",
+                        "primary_genre": "category",
+                        "total_number_of_ratings": "rating_count",
+                        "price_usd": "price",
+                    }
+                )
+                insert_columns = [
+                    "app_id",
+                    "app_name",
+                    "developer_id",
+                    "rating_count",
+                    "last_updated",
+                    "category",
+                    "platform",
+                    "store",
+                ]
+            upsert_df(
+                table_name="app_store_csv_dump",
+                insert_columns=insert_columns,
+                df=chunk,
+                key_columns=["platform", "store", "app_id"],
+            )
+            i += 1
