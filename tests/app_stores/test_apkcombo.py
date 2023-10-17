1e4e555df574fd10439cac268f704f116bd8051d
james <jamesoclaire@gmail.com>
2023-10-17 10:11:06 +0800

    Added test

--- tests/app_stores/test_apkcombo.py
+++ tests/app_stores/test_apkcombo.py
@@ -1,18 +1,24 @@
+import os
 import unittest
-from typing import Any
 from unittest.mock import patch
 
 from adscrawler.app_stores import apkcombo
 
 
 class TestApkcombo(unittest.TestCase):
-    # Mock the scrape_with_firefox function to return a sample list
-    @patch("adscrawler.app_stores.apkcombo.scrape_with_firefox")
-    def test_get_apkcombo_android_apps(self, mock_scrape: Any) -> None:
-        mock_scrape.return_value = ["sample_app_id_1", "sample_app_id_2"]
+    def test_get_apkcombo_android_apps(self) -> None:
+        use_mock = os.environ.get("USE_MOCK", "true").lower() == "true"
 
-        # Call the function
-        result = apkcombo.get_apkcombo_android_apps()
+        if use_mock:
+            with patch(
+                "adscrawler.app_stores.apkcombo.scrape_with_firefox"
+            ) as mock_scrape:
+                mock_scrape.return_value = ["sample_app_id_1", "sample_app_id_2"]
+                # Call the function
+                result = apkcombo.get_apkcombo_android_apps()
+        else:
+            # Call the function without mocking
+            result = apkcombo.get_apkcombo_android_apps()
 
         # Assert that the returned list has length greater than 0
         self.assertTrue(len(result) > 0)
