d0c857d4847557cd971c60d6674f88d7840f83be
james <jamesoclaire@gmail.com>
2023-10-28 13:48:23 +0800

    Add materialized view for most recent rankings

--- pg-ddl/db.sql
+++ pg-ddl/db.sql
@@ -1305,6 +1305,43 @@ CREATE UNIQUE INDEX idx_top_categories
 ON top_categories (store, mapped_category, store_id);
 
 
+CREATE MATERIALIZED VIEW app_rankings_most_recent AS
+SELECT 
+    country,
+    store,
+    store_collection,
+    store_category,
+    RANK,
+    MAX(crawled_date) AS max_crawled_date
+FROM
+    app_rankings ar
+GROUP BY 
+    country,
+    store,
+    store_collection,
+    store_category,
+    RANK
+ORDER BY 
+    country,
+    store,
+    store_collection,
+    store_category,
+    RANK
+;
+DROP INDEX IF EXISTS idx_app_rankings_most_recent;
+CREATE UNIQUE INDEX idx_app_rankings_most_recent
+ON
+app_rankings_most_recent (
+    country,
+    store,
+    store_collection,
+    store_category,
+    RANK,
+    max_crawled_date
+)
+;
+
+
 
 
 CREATE OR REPLACE FUNCTION public.pg_stat_statements(showtext boolean, OUT userid oid, OUT dbid oid, OUT toplevel boolean, OUT queryid bigint, OUT query text, OUT plans bigint, OUT total_plan_time double precision, OUT min_plan_time double precision, OUT max_plan_time double precision, OUT mean_plan_time double precision, OUT stddev_plan_time double precision, OUT calls bigint, OUT total_exec_time double precision, OUT min_exec_time double precision, OUT max_exec_time double precision, OUT mean_exec_time double precision, OUT stddev_exec_time double precision, OUT rows bigint, OUT shared_blks_hit bigint, OUT shared_blks_read bigint, OUT shared_blks_dirtied bigint, OUT shared_blks_written bigint, OUT local_blks_hit bigint, OUT local_blks_read bigint, OUT local_blks_dirtied bigint, OUT local_blks_written bigint, OUT temp_blks_read bigint, OUT temp_blks_written bigint, OUT blk_read_time double precision, OUT blk_write_time double precision, OUT temp_blk_read_time double precision, OUT temp_blk_write_time double precision, OUT wal_records bigint, OUT wal_fpi bigint, OUT wal_bytes numeric, OUT jit_functions bigint, OUT jit_generation_time double precision, OUT jit_inlining_count bigint, OUT jit_inlining_time double precision, OUT jit_optimization_count bigint, OUT jit_optimization_time double precision, OUT jit_emission_count bigint, OUT jit_emission_time double precision)
