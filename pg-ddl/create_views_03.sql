8247e0490d05eccf6186cf2174d25bfaf2858a6d
james <jamesoclaire@gmail.com>
2024-02-10 16:26:02 +0800

    Remove now unused top tracker

--- pg-ddl/create_views_03.sql
+++ pg-ddl/create_views_03.sql
@@ -19,14 +19,14 @@ ORDER BY
 CREATE MATERIALIZED VIEW apps_new_weekly AS
 WITH rankedapps AS (
     SELECT
-        *,
+        *,-- noqa: RF02
         ROW_NUMBER() OVER (
             PARTITION BY
-                store,
-                mapped_category
+                sa.store,
+                cm.mapped_category
             ORDER BY
-                installs DESC NULLS LAST,
-                rating_count DESC NULLS LAST
+                sa.installs DESC NULLS LAST,
+                sa.rating_count DESC NULLS LAST
         ) AS rn
     FROM
         store_apps AS sa
@@ -35,7 +35,7 @@ WITH rankedapps AS (
             sa.category = cm.original_category
     WHERE
         sa.release_date >= CURRENT_DATE - INTERVAL '7 days'
-        AND crawl_result = 1
+        AND sa.crawl_result = 1
 )
 
 SELECT *
@@ -55,14 +55,14 @@ ON apps_new_weekly (store, mapped_category, store_id);
 CREATE MATERIALIZED VIEW apps_new_monthly AS
 WITH rankedapps AS (
     SELECT
-        *,
+        *,-- noqa: RF02
         ROW_NUMBER() OVER (
             PARTITION BY
-                store,
-                mapped_category
+                sa.store,
+                cm.mapped_category
             ORDER BY
-                installs DESC NULLS LAST,
-                rating_count DESC NULLS LAST
+                sa.installs DESC NULLS LAST,
+                sa.rating_count DESC NULLS LAST
         ) AS rn
     FROM
         store_apps AS sa
@@ -71,7 +71,7 @@ WITH rankedapps AS (
             sa.category = cm.original_category
     WHERE
         sa.release_date >= CURRENT_DATE - INTERVAL '30 days'
-        AND crawl_result = 1
+        AND sa.crawl_result = 1
 )
 
 SELECT *
@@ -89,14 +89,14 @@ DROP MATERIALIZED VIEW IF EXISTS apps_new_yearly;
 CREATE MATERIALIZED VIEW apps_new_yearly AS
 WITH rankedapps AS (
     SELECT
-        *,
+        *,-- noqa: RF02
         ROW_NUMBER() OVER (
             PARTITION BY
-                store,
-                mapped_category
+                sa.store,
+                cm.mapped_category
             ORDER BY
-                installs DESC NULLS LAST,
-                rating_count DESC NULLS LAST
+                sa.installs DESC NULLS LAST,
+                sa.rating_count DESC NULLS LAST
         ) AS rn
     FROM
         store_apps AS sa
@@ -105,7 +105,7 @@ WITH rankedapps AS (
             sa.category = cm.original_category
     WHERE
         sa.release_date >= CURRENT_DATE - INTERVAL '365 days'
-        AND crawl_result = 1
+        AND sa.crawl_result = 1
 )
 
 SELECT *
@@ -124,14 +124,14 @@ ON apps_new_yearly (store, mapped_category, store_id);
 CREATE MATERIALIZED VIEW top_categories AS
 WITH rankedapps AS (
     SELECT
-        *,
+        *, -- noqa: RF02
         ROW_NUMBER() OVER (
             PARTITION BY
-                store,
-                mapped_category
+                sa.store,
+                cm.mapped_category
             ORDER BY
-                installs DESC NULLS LAST,
-                rating_count DESC NULLS LAST
+                sa.installs DESC NULLS LAST,
+                sa.rating_count DESC NULLS LAST
         ) AS rn
     FROM
         store_apps AS sa
@@ -153,57 +153,3 @@ WITH DATA;
 DROP INDEX IF EXISTS idx_top_categories;
 CREATE UNIQUE INDEX idx_top_categories
 ON top_categories (store, mapped_category, store_id);
-
-
-
-CREATE MATERIALIZED VIEW top_trackers AS
-WITH latest_version_codes AS (
-    SELECT
-        id,
-        store_app,
-        MAX(version_code) AS version_code
-    FROM
-        version_codes
-    GROUP BY
-        id,
-        store_app
-),
-
-trackers_per_app AS (
-    SELECT DISTINCT
-        vc.store_app,
-        vc.version_code,
-        tm.tracker
-    FROM
-        version_details AS vd
-    LEFT JOIN latest_version_codes AS vc
-        ON
-            vd.version_code = vc.id
-    LEFT JOIN tracker_package_map AS tm ON
-        vd.android_name ~* tm.package_pattern
-),
-
-tracker_counts AS (
-    SELECT
-        tracker,
-        COUNT(DISTINCT store_app)
-    FROM
-        trackers_per_app
-    GROUP BY
-        tracker
-)
-
-SELECT
-    t.id AS tracker,
-    t.name AS tracker_name,
-    tc.count
-FROM
-    tracker_counts AS tc
-LEFT JOIN trackers AS t
-    ON
-        tc.tracker = t.id
-WITH DATA;
-
-DROP INDEX IF EXISTS idx_top_trackers;
-CREATE UNIQUE INDEX idx_top_trackers
-ON top_trackers (tracker);
