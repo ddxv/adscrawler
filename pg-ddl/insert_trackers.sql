b933c0655d25c36be3637d33966eac7b1b90cf08
james <jamesoclaire@gmail.com>
2024-02-16 15:34:35 +0800

    Add more sdk trackers

--- pg-ddl/insert_trackers.sql
+++ pg-ddl/insert_trackers.sql
@@ -8,7 +8,8 @@ INSERT INTO trackers (id, name) VALUES
 (7, 'IAB Open Measurement'),
 (8, 'Tenjin'),
 (9, 'AirBridge'),
-(10, 'No Trackers Found');
+(10, 'No Trackers Found'),
+(11, 'WanMei 完美');
 
 INSERT INTO networks (id, name) VALUES
 (1, 'Google AdMob'),
@@ -27,7 +28,9 @@ INSERT INTO networks (id, name) VALUES
 (14, 'ChartBoost'),
 (15, 'TapJoy'),
 (16, 'Fyber'),
-(17, 'No Ad Network Detected');
+(17, 'No Ad Network Detected'),
+(18, 'VK'),
+(19, 'AdJoe');
 
 
 INSERT INTO public.tracker_package_map (tracker, package_pattern) VALUES
@@ -50,7 +53,9 @@ INSERT INTO public.tracker_package_map (tracker, package_pattern) VALUES
 (7, 'com.prime31.util.IabHelperImpl'),
 (7, 'com.prime31.IAB'),
 (8, 'com.tenjin.android'),
-(9, 'io.airbridge');
+(9, 'io.airbridge'),
+(11, 'com.wpsdk'),
+(1, 'com.google.firebase');
 
 
 INSERT INTO public.network_package_map (network, package_pattern) VALUES
@@ -75,7 +80,8 @@ INSERT INTO public.network_package_map (network, package_pattern) VALUES
 (8, 'com.vungle.warren'),
 (9, 'com.amazon.device.ads'),
 (10, 'com.onesignal'),
-(11, 'com.flurry');
+(11, 'com.flurry'),
+(5, 'com.facebook.sdk.ApplicationId');
 
 INSERT INTO public.network_package_map (network, package_pattern) VALUES
 (12, 'com.bytedance'),
@@ -85,4 +91,7 @@ INSERT INTO public.network_package_map (network, package_pattern) VALUES
 (13, 'com.mbridge.msdk'),
 (14, 'com.chartboost.sdk'),
 (15, 'com.tapjoy'),
-(16, 'com.fyber');
+(16, 'com.fyber'),
+(18, 'com.vk.sdk'),
+(18, 'com.vk.api.sdk'),
+(19, 'io.adjoe.sdk');
