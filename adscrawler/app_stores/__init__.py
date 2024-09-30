04063c4c4df45a092d7e834478e5194924d92091
james <jamesoclaire@gmail.com>
2024-09-30 15:36:59 +0800

    try exposing modules

--- adscrawler/app_stores/__init__.py
+++ adscrawler/app_stores/__init__.py
@@ -1,4 +1,5 @@
 # adscrawler/app_stores/__init__.py
 import adscrawler.app_stores.google as google
+import adscrawler.app_stores.scrape_stores as scrape_stores
 
-__all__ = ['google']
+__all__ = ['google', 'scrape_stores']
