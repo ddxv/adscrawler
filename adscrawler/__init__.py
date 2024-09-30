983f219888c2f441513a930eed09dda37ce00387
james <jamesoclaire@gmail.com>
2024-09-30 14:57:02 +0800

    Try adding imports in __init__.py

--- adscrawler/__init__.py
+++ adscrawler/__init__.py
@@ -1,3 +1,3 @@
 from . import config, connection, queries, scrape, tools
 
-__all__ = ['config', 'connection', 'queries', 'scrape', 'tools']
+__all__ = ['config', 'connection', 'queries', 'scrape', 'tools', 'app_stores']
