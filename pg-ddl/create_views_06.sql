5b94eb6bd0938269eb7649c3cfe8672b78787817
James O'Claire <jamesoclaire@gmail.com>
2024-12-13 10:34:07 +0800

    Added stores_app_overview for frontend

--- pg-ddl/create_views_06.sql
+++ pg-ddl/create_views_06.sql
@@ -1,4 +1,4 @@
-CREATE MATERIALIZED VIEW latest_version_codes_mv AS
+CREATE MATERIALIZED VIEW store_apps_overview AS
 WITH latest_version_codes AS (
     SELECT DISTINCT ON
     (vc.store_app)
@@ -9,23 +9,53 @@ WITH latest_version_codes AS (
         vc.crawl_result
     FROM
         version_codes AS vc
-    ORDER BY
-        vc.store_app,
-        vc.version_code::TEXT DESC
 )
 
 SELECT
-    sa.id AS store_app,
+    sa.id,
+    sa.name,
     sa.store_id,
-    lvc.updated_at AS crawled_at,
+    sa.store,
+    sa.category,
+    sa.rating,
+    sa.rating_count,
+    sa.review_count,
+    sa.installs,
+    sa.store_last_updated,
+    sa.created_at,
+    sa.updated_at,
+    sa.crawl_result,
+    sa.icon_url_512,
+    sa.release_date,
+    sa.featured_image_url,
+    sa.phone_image_url_1,
+    sa.phone_image_url_2,
+    sa.phone_image_url_3,
+    sa.tablet_image_url_1,
+    sa.tablet_image_url_2,
+    sa.tablet_image_url_3,
+    d.developer_id,
+    d.name AS developer_name,
+    pd.url AS developer_url,
+    pd.updated_at AS adstxt_last_crawled,
+    pd.crawl_result AS adstxt_crawl_result,
     lvc.version_code,
-    lvc.crawl_result
+    lvc.updated_at AS sdk_last_crawled,
+    lvc.crawl_result AS sdk_crawl_result
 FROM
-    latest_version_codes AS lvc
-LEFT JOIN
     store_apps AS sa
+LEFT JOIN developers AS d
     ON
-        lvc.store_app = sa.id;
+        sa.developer = d.id
+LEFT JOIN app_urls_map AS aum
+    ON
+        sa.id = aum.store_app
+LEFT JOIN pub_domains AS pd
+    ON
+        aum.pub_domain = pd.id
+LEFT JOIN latest_version_codes AS lvc
+    ON
+        sa.id = lvc.store_app
+WITH DATA;
 
-
-CREATE INDEX latest_version_codes_mv_idx ON latest_version_codes_mv (store_id);
+CREATE INDEX store_apps_overview_idx ON store_apps_overview (store_id);
