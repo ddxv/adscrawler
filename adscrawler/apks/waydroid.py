f61beef070b6dbce85342461847d1b3e96a92bdb
James O'Claire <jamesoclaire@gmail.com>
2025-04-26 10:56:11 +0800

    Separate out waydroid function to own file

---
+++ adscrawler/apks/waydroid.py
@@ -0,0 +1,306 @@
+import os
+import pathlib
+import subprocess
+import time
+
+import pandas as pd
+
+from adscrawler.apks import mitm_process_log
+from adscrawler.config import (
+    ANDROID_SDK,
+    APKS_DIR,
+    PACKAGE_DIR,
+    XAPKS_DIR,
+    get_logger,
+)
+from adscrawler.connection import PostgresCon
+
+logger = get_logger(__name__)
+
+
+def run_app(database_connection: PostgresCon, extension: str, row: pd.Series) -> None:
+    store_id = row.store_id
+    function_info = f"waydroid {store_id=}"
+    store_app = row.store_app
+    apk_path = pathlib.Path(APKS_DIR, f"{store_id}.apk")
+    if extension == ".xapk":
+        xapk_path = pathlib.Path(XAPKS_DIR, f"{store_id}{extension}")
+        os.system(f"java -jar APKEditor.jar m -i {xapk_path.as_posix()}")
+        # APKEditor merged APKs must be signed to install
+        apk_path = pathlib.Path(APKS_DIR, f"{store_id}.apk")
+        merged_apk_path = pathlib.Path(XAPKS_DIR, f"{store_id}_merged.apk")
+        os.system(
+            f"{ANDROID_SDK}/apksigner sign --ks ~/.android/debug.keystore  --ks-key-alias androiddebugkey   --ks-pass pass:android   --key-pass pass:android   --out {apk_path}  {merged_apk_path}"
+        )
+        apk_path.exists()
+
+    logger.info(f"{function_info} clearing mitmdump")
+    mitm_script = pathlib.Path(PACKAGE_DIR, "adscrawler/apks/mitm_start.sh")
+    os.system(f"{mitm_script.as_posix()} -d")
+
+    _mitm_process = launch_and_track_app(store_id, apk_path)
+
+    mdf = mitm_process_log.parse_mitm_log(store_id)
+    logger.info(f"{function_info} MITM log has {mdf.shape[0]} rows")
+    if mdf.empty:
+        logger.warning(f"{function_info} MITM log returned empty dataframe")
+        return
+    mdf = mdf.rename(columns={"timestamp": "crawled_at"})
+    mdf["url"] = mdf["url"].str[0:1000]
+    mdf = mdf[["url", "host", "crawled_at", "status_code", "tld_url"]].drop_duplicates()
+    mdf["store_app"] = store_app
+    mdf.to_sql(
+        name="store_app_api_calls",
+        con=database_connection.engine,
+        if_exists="append",
+        index=None,
+    )
+    logger.info(f"{function_info} saved to db")
+
+
+def check_session() -> bool:
+    waydroid_process = subprocess.run(
+        ["waydroid", "status"], capture_output=True, text=True, check=False
+    )
+    # Look specifically for the line "Session:  RUNNING"
+    for line in waydroid_process.stdout.splitlines():
+        print(line.strip())
+        if line.strip() == "Session:\tRUNNING":
+            return True
+    return False
+
+
+def restart() -> tuple[subprocess.Popen, subprocess.Popen]:
+    os.system("waydroid session stop")
+
+    # This is required to run weston in cronjob environment
+    os.environ["XDG_RUNTIME_DIR"] = "/run/user/1000"
+
+    if not check_wayland_display():
+        _weston_process = start_weston()
+
+    waydroid_process = start_session()
+    if not waydroid_process:
+        logger.error("Waydroid failed to start")
+        return
+    return waydroid_process
+
+
+def check_wayland_display() -> bool:
+    return os.environ.get("WAYLAND_DISPLAY") is not None
+
+
+def launch_and_track_app(store_id: str, apk_path: pathlib.Path) -> None:
+    function_info = f"waydroid {store_id=} launch and track"
+    mitm_script = pathlib.Path(PACKAGE_DIR, "adscrawler/apks/mitm_start.sh")
+
+    install_app(store_id, apk_path)
+    logger.info(
+        f"{function_info} starting mitmdump with script {mitm_script.as_posix()}"
+    )
+    mitm_logfile = pathlib.Path(PACKAGE_DIR, f"mitmlogs/traffic_{store_id}.log")
+    mitm_process = subprocess.Popen(
+        [f"{mitm_script.as_posix()}", "-w", "-s", mitm_logfile.as_posix()],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+    )
+    # Store the PID
+    mitm_pid = mitm_process.pid
+    logger.info(f"{function_info} mitmdump started with PID: {mitm_pid}")
+
+    launch_app(store_id)
+
+    logger.info(f"{function_info} waiting for 60 seconds")
+    time.sleep(60)
+    logger.info(f"{function_info} stopping app & mitmdump")
+    os.system(f"{mitm_script.as_posix()} -d")
+    os.system(f'sudo waydroid shell am force-stop "{store_id}"')
+    os.system(f'waydroid app remove "{store_id}"')
+    logger.info(f"{function_info} success")
+
+
+def launch_app(store_id: str) -> None:
+    function_info = f"waydroid {store_id=} launch"
+    logger.info(f"{function_info} start")
+    os.system(f'waydroid app launch "{store_id}"')
+
+    time.sleep(2)
+
+    # Set timeout parameters
+    timeout = 60  # seconds
+    start_time = time.time()
+    found = False
+
+    # Loop until timeout or app is found
+    while time.time() - start_time < timeout and not found:
+        # Run the waydroid shell dumpsys command
+        result = subprocess.run(
+            ["sudo", "waydroid", "shell", "dumpsys", "activity", "activities"],
+            capture_output=True,
+            text=True,
+            check=False,
+        )
+
+        # Check if app is in the output
+        if store_id in result.stdout:
+            found = True
+            logger.info(f"{function_info} in foreground")
+            break
+
+        # Wait before launching again
+        logger.info(f"{function_info} not in foreground, relaunching")
+        os.system(f'waydroid app launch "{store_id}"')
+        time.sleep(2)
+
+    if not found:
+        logger.error(
+            f"{function_info} not found in the foreground after {timeout} seconds"
+        )
+        raise Exception(f"waydroid {store_id=} failed to launch")
+    logger.info(f"{function_info} success")
+
+
+def install_app(store_id: str, apk_path: pathlib.Path) -> None:
+    function_info = f"Waydroid install {store_id=}"
+    logger.info(f"{function_info} checking")
+    applist = subprocess.run(
+        ["waydroid", "app", "list"], capture_output=True, text=True, check=False
+    )
+
+    output = applist.stdout
+
+    if store_id in output:
+        logger.info(f"{function_info} found already installed")
+        return
+    logger.info(f"{function_info} installing")
+
+    _install_output = subprocess.run(
+        ["waydroid", "app", "install", apk_path.as_posix()],
+        capture_output=True,
+        text=True,
+        check=False,
+    )
+
+    time.sleep(2)
+
+    timeout = 30
+    start_time = time.time()
+    while (time.time() - start_time) < timeout:
+        applist = subprocess.run(
+            ["waydroid", "app", "list"], capture_output=True, text=True, check=False
+        )
+        output = applist.stdout
+        if store_id in output:
+            logger.info(f"{function_info} installed")
+            return
+        time.sleep(1)
+
+    applist = subprocess.run(
+        ["waydroid", "app", "list"], capture_output=True, text=True, check=False
+    )
+
+    output = applist.stdout
+
+    if store_id in output:
+        logger.info(f"{function_info} installed")
+        return
+
+    raise Exception(f"Waydroid failed to install {store_id}")
+
+
+def start_session() -> subprocess.Popen:
+    function_info = "Waydroid session"
+    logger.info(f"{function_info} start")
+    # Start the Waydroid session process
+    waydroid_process = subprocess.Popen(
+        ["waydroid", "session", "start"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+        bufsize=1,
+    )
+
+    # Set a timeout (in seconds)
+    timeout = 120  # Wait up to 2 minutes
+    start_time = time.time()
+    ready = False
+    display_waiting = True
+
+    while (
+        waydroid_process.poll() is None
+        and not ready
+        and (time.time() - start_time) < timeout
+    ):
+        line = waydroid_process.stdout.readline()
+        logger.info(line)
+        if (
+            "Android with user 0 is ready" in line
+            or "Session is already running" in line
+        ):
+            ready = True
+            logger.info("Waydroid is ready! Continuing with the script...")
+            break
+        if display_waiting:
+            logger.info(f"{function_info} waiting for ready (120 seconds)...")
+            display_waiting = False
+        time.sleep(1)
+
+    if not ready:
+        if waydroid_process.poll() is not None:
+            logger.error("Waydroid process ended without becoming ready")
+        else:
+            logger.error(
+                f"Timed out after {timeout} seconds waiting for Waydroid to be ready"
+            )
+            waydroid_process.terminate()
+    logger.info(f"{function_info} success")
+    return waydroid_process
+
+
+def start_weston() -> subprocess.Popen:
+    # This will be the socket name for the weston process
+    socket_name = "wayland-98"
+    os.environ["WAYLAND_DISPLAY"] = socket_name
+
+    weston_process = subprocess.Popen(
+        [
+            "weston",
+            "-B",
+            "headless",
+            "--width=800",
+            "--height=800",
+            "--scale=1",
+            "-S",
+            socket_name,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+        bufsize=1,
+    )
+    ready = False
+    timeout = 30
+    start_time = time.time()
+    while (
+        weston_process.poll() is None
+        and not ready
+        and (time.time() - start_time) < timeout
+    ):
+        line = weston_process.stdout.readline()
+        logger.info(line)
+        if "launching '/usr/libexec/weston-desktop-shell'" in line:
+            ready = True
+            logger.info("Weston is ready! Continuing with the script...")
+            break
+
+    if not ready:
+        if weston_process.poll() is not None:
+            logger.error("Weston process ended without becoming ready")
+        else:
+            logger.error(
+                f"Timed out after {timeout} seconds waiting for Waydroid to be ready"
+            )
+            weston_process.terminate()
+        return
+    return weston_process
