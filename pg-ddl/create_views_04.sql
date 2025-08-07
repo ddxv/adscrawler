25a3288e0dffcc9dd6c0115c7e651b311f503921
James O'Claire <jamesoclaire@gmail.com>
2025-08-07 14:52:03 +0800

    new combined_store_apps_companies

--- pg-ddl/create_views_04.sql
+++ pg-ddl/create_views_04.sql
@@ -1,6 +1,57 @@
+
+SELECT
+    DISTINCT saac.store_app,
+    cm.mapped_category AS app_category,
+    cdm.company_id,
+    c.parent_company_id AS parent_id,
+    saac.tld_url AS ad_domain,
+    'api_call' AS tag_source
+FROM
+    store_app_api_calls saac
+LEFT JOIN store_apps sa ON
+    saac.store_app = sa.id
+LEFT JOIN
+        category_mapping AS cm
+        ON
+    sa.category::text = cm.original_category::text
+LEFT JOIN ad_domains ad ON
+    saac.tld_url = ad."domain"
+LEFT JOIN adtech.company_domain_mapping cdm ON
+    ad.id = cdm.domain_id
+LEFT JOIN adtech.companies c ON
+    cdm.company_id = c.id;
+
+    sdk_based_companies.company_id,
+    sdk_based_companies.parent_id,
+    sdk_based_companies.ad_domain,
+    sdk_based_companies.tag_source
+
+
 CREATE MATERIALIZED VIEW adtech.combined_store_apps_companies
 TABLESPACE pg_default
-AS WITH sdk_based_companies AS (
+AS WITH
+api_based_companies AS (SELECT
+    DISTINCT saac.store_app,
+    cm.mapped_category AS app_category,
+    cdm.company_id,
+    c.parent_company_id AS parent_id,
+    saac.tld_url AS ad_domain,
+    'api_call' AS tag_source
+FROM
+    store_app_api_calls saac
+LEFT JOIN store_apps sa ON
+    saac.store_app = sa.id
+LEFT JOIN
+        category_mapping AS cm
+        ON
+    sa.category::text = cm.original_category::text
+LEFT JOIN ad_domains ad ON
+    saac.tld_url = ad."domain"
+LEFT JOIN adtech.company_domain_mapping cdm ON
+    ad.id = cdm.domain_id
+LEFT JOIN adtech.companies c ON
+    cdm.company_id = c.id),
+sdk_based_companies AS (
     SELECT
         sac.store_app,
         cm.mapped_category AS app_category,
@@ -16,7 +67,6 @@ AS WITH sdk_based_companies AS (
         category_mapping AS cm
         ON sa.category::text = cm.original_category::text
 ),
-
 distinct_ad_and_pub_domains AS (
     SELECT DISTINCT
         pd.url AS publisher_domain_url,
@@ -28,7 +78,6 @@ distinct_ad_and_pub_domains AS (
     LEFT JOIN pub_domains AS pd ON aam.pub_domain = pd.id
     WHERE (pd.crawled_at - aam.updated_at) < '01:00:00'::interval
 ),
-
 adstxt_based_companies AS (
     SELECT DISTINCT
         aum.store_app,
@@ -59,8 +108,17 @@ adstxt_based_companies AS (
     WHERE
         sa.crawl_result = 1
         AND (pnv.ad_domain_url IS NOT NULL OR c.id IS NOT NULL)
-)
-
+),
+combined_sources AS (
+SELECT
+    api_based_companies.store_app,
+    api_based_companies.app_category,
+    api_based_companies.company_id,
+    api_based_companies.parent_id,
+    api_based_companies.ad_domain,
+    api_based_companies.tag_source
+FROM api_based_companies
+UNION ALL
 SELECT
     sdk_based_companies.store_app,
     sdk_based_companies.app_category,
@@ -69,7 +127,7 @@ SELECT
     sdk_based_companies.ad_domain,
     sdk_based_companies.tag_source
 FROM sdk_based_companies
-UNION
+UNION ALL
 SELECT
     adstxt_based_companies.store_app,
     adstxt_based_companies.app_category,
@@ -77,8 +135,40 @@ SELECT
     adstxt_based_companies.parent_id,
     adstxt_based_companies.ad_domain,
     adstxt_based_companies.tag_source
-FROM adstxt_based_companies
+FROM adstxt_based_companies)
+SELECT
+    cs.ad_domain,
+    cs.store_app,
+    sa.category AS app_category,
+    c.id AS company_id,
+    COALESCE(c.parent_company_id, c.id) AS parent_id,
+    CASE
+        WHEN sa.sdk_successful_last_crawled IS NOT NULL THEN bool_or(cs.tag_source = 'sdk')
+        ELSE NULL
+    END AS sdk,
+    CASE
+        WHEN sa.api_successful_last_crawled IS NOT NULL THEN bool_or(cs.tag_source = 'api_call')
+        ELSE NULL
+    END AS api_call,
+    bool_or(cs.tag_source = 'app_ads_direct') AS app_ads_direct,
+    bool_or(cs.tag_source = 'app_ads_reseller') AS app_ads_reseller
+FROM combined_sources cs
+LEFT JOIN frontend.store_apps_overview sa ON cs.store_app = sa.id
+LEFT JOIN ad_domains ad ON cs.ad_domain = ad.domain
+LEFT JOIN adtech.companies c ON ad.id = c.domain_id
+GROUP BY
+    cs.ad_domain,
+    cs.store_app,
+    sa.category,
+    c.id,
+    c.parent_company_id,
+    sa.sdk_successful_last_crawled,
+    sa.api_successful_last_crawled
 WITH DATA;
+
+
+CREATE UNIQUE INDEX combined_store_app_companies_idx ON adtech.combined_store_apps_companies USING btree (ad_domain, store_app, app_category, company_id);
+
 
 
 CREATE MATERIALIZED VIEW adtech.combined_store_apps_parent_companies
