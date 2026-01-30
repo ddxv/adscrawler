7a1700f678eaac54c14a3b66f5314016e9d2bd83
James O'Claire <james@oclaire.dev>
2026-01-30 10:43:46 +0800

    Use new logging table to cut down on query time for apps to keyword extract

--- adscrawler/dbcon/sql/query_apps_to_process_keywords.sql
+++ adscrawler/dbcon/sql/query_apps_to_process_keywords.sql
@@ -1,6 +1,5 @@
-WITH latest_descriptions AS (
-    SELECT DISTINCT ON
-    (sad.store_app)
+ WITH latest_descriptions AS (
+    SELECT DISTINCT ON (sad.store_app)
         sad.id AS description_id,
         sad.store_app,
         sad.description_short,
@@ -14,50 +13,52 @@ WITH latest_descriptions AS (
         sad.store_app ASC,
         sad.updated_at DESC
 ),
-latest_extractions AS (
+ latest_extractions AS (
     SELECT DISTINCT ON
-    (ak.store_app)
-        ak.store_app,
-        ak.extracted_at AS last_extracted_at
+        (adke.description_id)
+        adke.description_id,
+        adke.store_app,
+        adke.extracted_at AS app_keywords_extracted_at
     FROM
-        app_keywords_extracted AS ak
+        logging.app_description_keywords_extracted AS adke
     ORDER BY
-        ak.store_app ASC,
-        ak.extracted_at DESC
+        adke.description_id ASC,
+        adke.extracted_at DESC
 ),
 base AS (
     SELECT
         ld.store_app,
         ld.description_id,
-        le.last_extracted_at,
+        le.app_keywords_extracted_at,
         ld.description_short,
         ld.description
     FROM latest_descriptions AS ld
     LEFT JOIN
         latest_extractions AS le
         ON
-            ld.store_app = le.store_app
-    WHERE le.last_extracted_at IS NULL OR (
-        ld.description_last_updated > le.last_extracted_at
-        AND le.last_extracted_at <= NOW() - INTERVAL '7 days'
+            ld.description_id = le.description_id
+            WHERE
+le.app_keywords_extracted_at IS NULL
+OR ld.description_last_updated > le.app_keywords_extracted_at
+OR le.app_keywords_extracted_at <= NOW() - INTERVAL '31 days'
     )
 )
-SELECT
+SELECT	
     b.store_app,
     b.description_id,
-    b.last_extracted_at,
+    b.app_keywords_extracted_at,
     b.description_short,
     b.description
 FROM
     base AS b
 INNER JOIN app_global_metrics_latest AS agml ON b.store_app = agml.store_app
 ORDER BY
-    (CASE WHEN b.last_extracted_at IS NULL THEN 1 ELSE 0 END) DESC, -- always crawl new ones first
+    (CASE WHEN b.app_keywords_extracted_at IS NULL THEN 1 ELSE 0 END) DESC, -- always crawl new ones first
     (
         GREATEST(
             COALESCE(agml.installs, 0),
             COALESCE(agml.rating_count::BIGINT, 0)
         )
-        * (10 * COALESCE(EXTRACT(DAY FROM (NOW() - b.last_extracted_at)), 1))
+        * (10 * COALESCE(EXTRACT(DAY FROM (NOW() - b.app_keywords_extracted_at)), 1))
     ) DESC
 LIMIT :mylimit;
