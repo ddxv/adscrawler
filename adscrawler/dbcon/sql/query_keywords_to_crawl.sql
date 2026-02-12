a4c49f186e544e4f692524f48c76fe2b9cbe337b
James O'Claire <james@oclaire.dev>
2026-02-12 23:59:42 +0800

    Use search queries top priority for keyword searches

--- adscrawler/dbcon/sql/query_keywords_to_crawl.sql
+++ adscrawler/dbcon/sql/query_keywords_to_crawl.sql
@@ -1,7 +1,5 @@
 WITH rank_crawled_keywords AS (
-    SELECT DISTINCT
-        akr.store,
-        akr.keyword_id
+    SELECT DISTINCT akr.keyword_id
     FROM
         frontend.app_keyword_ranks_daily AS akr
     WHERE
@@ -13,28 +11,59 @@ log_crawled_keywords AS (
         logging.keywords_crawled_at
     WHERE
         crawled_at > CURRENT_DATE - INTERVAL '7 days'
+),
+scheduled_keywords AS (
+    SELECT
+        keyword_id,
+        keyword_text,
+        app_count,
+        total_apps
+    FROM
+        frontend.keyword_scores
+    WHERE
+        keyword_id NOT IN (
+            SELECT rck.keyword_id
+            FROM
+                rank_crawled_keywords AS rck
+        )
+        OR keyword_id NOT IN (
+            SELECT lck.keyword
+            FROM
+                log_crawled_keywords AS lck
+        )
+    ORDER BY
+        competitiveness_score
+        DESC
+),
+distinct_sq AS (
+    SELECT DISTINCT search_term
+    FROM
+        agadmin.search_queries
 )
 SELECT
-    store,
-    keyword_id,
-    keyword_text,
-    app_count,
-    total_apps,
-    competitiveness_score
+    k.id AS keyword_id,
+    k.keyword_text,
+    'user' AS priority,
+    0 AS app_count,
+    0 AS total_apps
 FROM
-    frontend.keyword_scores
+    distinct_sq AS sq
+LEFT JOIN keywords AS k
+    ON
+        sq.search_term = k.keyword_text
 WHERE
-    keyword_id NOT IN (
-        SELECT rck.keyword_id
-        FROM
-            rank_crawled_keywords AS rck
-    )
-    OR keyword_id NOT IN (
+    k.id NOT IN (
         SELECT lck.keyword
         FROM
             log_crawled_keywords AS lck
     )
-ORDER BY
-    competitiveness_score
-    DESC
+UNION ALL
+SELECT
+    keyword_id,
+    keyword_text,
+    'scheduled' AS priority,
+    app_count,
+    total_apps
+FROM
+    scheduled_keywords
 LIMIT :mylimit;
