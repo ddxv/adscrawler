d3578bf7cdfd4a02a4bdbe7d6a9f5c32b0d0ad19
James O'Claire <james@oclaire.dev>
2026-06-16 12:16:46 +0800

    Speed up apps_to_update for primary group

--- adscrawler/dbcon/sql/query_apps_to_update_primary.sql
+++ adscrawler/dbcon/sql/query_apps_to_update_primary.sql
@@ -14,20 +14,74 @@ WITH countries_to_crawl AS (
     WHERE
         s.name = 'app_details'
         AND cc.enabled = TRUE
-        AND cc.priority = :country_crawl_priority
+        -- Just returns one country US
+        AND cc.priority = 1
+),
+ranked_apps AS (
+    SELECT store_app FROM store_apps_in_latest_rankings
+),
+target_apps AS (
+    SELECT
+        sa.id,
+        sa.store,
+        sa.store_id,
+        sa.store_last_updated,
+        sa.crawl_result,
+        sa.updated_at,
+        sa.created_at,
+        sa.release_date,
+        sa.icon_url_100,
+        sa.additional_html_scraped_at,
+        CASE WHEN sa.crawl_result IS NULL THEN 1 ELSE 0 END AS crawl_result_null,
+        CASE WHEN ra.store_app IS NOT NULL THEN 1 ELSE 0 END AS ranked_app,
+        -- carry agm values in so we don't need to join it again later
+        COALESCE(agm.total_installs, 0) AS total_installs,
+        COALESCE(agm.total_ratings, 0)  AS total_ratings
+    FROM store_apps sa
+    LEFT JOIN ranked_apps ra ON ra.store_app = sa.id
+    LEFT JOIN app_global_metrics_latest agm ON agm.store_app = sa.id
+    WHERE sa.store = :store
+      AND (
+          sa.crawl_result IS NULL
+          OR (
+              (
+                  agm.total_installs >= :short_update_installs
+                  OR agm.total_ratings >= :short_update_ratings
+                  OR ra.store_app IS NOT NULL
+                  OR sa.release_date > NOW() - INTERVAL '30 days'
+              )
+              AND sa.updated_at <= :short_update_ts
+              AND (
+                  sa.crawl_result = 1
+                  OR sa.crawl_result IS NULL
+                  OR sa.created_at >= :long_update_ts
+                  OR sa.store_last_updated >= :year_ago_ts
+              )
+          )
+          OR (
+              sa.updated_at <= :long_update_ts
+              AND (
+                  sa.crawl_result = 1
+                  OR sa.crawl_result IS NULL
+                  OR sa.store_last_updated >= :year_ago_ts
+              )
+          )
+          OR sa.updated_at <= :max_recrawl_ts
+      )
 ),
 latest_crawls AS (
     SELECT DISTINCT ON
     (
-        store_app,
-        country_id
+        acc.store_app,
+        acc.country_id
     )
-        store_app,
-        country_id,
-        crawled_at,
-        crawl_result
+        acc.store_app,
+        acc.country_id,
+        acc.crawled_at
     FROM
-        logging.app_country_crawls
+        logging.app_country_crawls acc
+    INNER JOIN target_apps sa ON sa.id = acc.store_app
+    WHERE crawled_at >= :max_recrawl_ts AND country_id IN (SELECT country_id FROM countries_to_crawl)
     ORDER BY
         store_app ASC,
         country_id ASC,
@@ -46,82 +100,18 @@ SELECT
     sa.updated_at AS app_updated_at,
     lc.crawled_at AS country_crawled_at
 FROM
-    public.store_apps AS sa
+    target_apps AS sa
 CROSS JOIN countries_to_crawl AS ctc
-LEFT JOIN app_global_metrics_latest AS agm
-    ON
-        sa.id = agm.store_app
 LEFT JOIN latest_crawls AS lc
     ON
         sa.id = lc.store_app
         AND ctc.country_id = lc.country_id
-WHERE
-    sa.store = :store
-    AND (
-        -- Always crawl new apps
-        sa.crawl_result IS NULL
-        -- Short update conditions
-        OR (
-            (
-                agm.total_installs >= :short_update_installs
-                OR agm.total_ratings >= :short_update_ratings
-                OR (
-                    sa.id IN (
-                        SELECT sailr.store_app
-                        FROM
-                            store_apps_in_latest_rankings AS sailr
-                    )
-                    OR release_date > NOW() - INTERVAL '30 days'
-                )
-            )
-            AND sa.updated_at <= :short_update_ts
-            AND (
-                sa.crawl_result = 1
-                OR sa.crawl_result IS NULL
-                OR sa.created_at >= :long_update_ts
-                OR sa.store_last_updated >= :year_ago_ts
-            )
-        )
-        -- Long update conditions
-        OR (
-            sa.updated_at <= :long_update_ts
-            AND (
-                sa.crawl_result = 1
-                OR sa.crawl_result IS NULL
-                OR sa.store_last_updated >= :year_ago_ts
-            )
-        )
-        -- Max update conditions
-        OR (
-            sa.updated_at <= :max_recrawl_ts
-        )
-    )
 ORDER BY
-    (
-        CASE
-            WHEN sa.crawl_result IS NULL THEN 1
-            ELSE 0
-        END
-    ) DESC,
     -- always crawl new ones first
-    (
-        CASE
-            WHEN sa.id IN (
-                SELECT sailr.store_app
-                FROM
-                    store_apps_in_latest_rankings AS sailr
-            ) THEN 1
-            ELSE 0
-        END
-    ) DESC,
+    crawl_result_null DESC,
     --always crawl apps in rankings, sometimes new apps get missed
-    (
-        GREATEST(
-            COALESCE(agm.total_installs, 0),
-            COALESCE(agm.total_ratings, 0)
-        )
-        * (
-            10 * EXTRACT(DAY FROM (NOW() - sa.updated_at))
-        )
-    ) DESC
-LIMIT :mylimit;
+    ranked_app DESC,
+    (GREATEST(total_installs, total_ratings)
+        * 10 * EXTRACT(DAY FROM (NOW() - sa.updated_at))) DESC
+LIMIT :mylimit
+;
