728e98bfbfd9086a24dc05c86b63213d3fb673c3
James O'Claire <james@oclaire.dev>
2026-07-19 12:04:16 +0800

    Move parent id to correct table

--- adscrawler/dbcon/sql/duckdb/cache_pg_tables.sql
+++ adscrawler/dbcon/sql/duckdb/cache_pg_tables.sql
@@ -4,9 +4,9 @@ ATTACH '{db_uri}' AS pg (TYPE postgres);
 
 CREATE TEMP TABLE domains_cache AS SELECT id, domain_name FROM pg.domains;
 
-CREATE TEMP TABLE companies_cache AS SELECT id, domain_id FROM pg.adtech.companies;
+CREATE TEMP TABLE companies_cache AS SELECT id, domain_id, parent_company_id FROM pg.adtech.companies;
 
-CREATE TEMP TABLE company_domain_mapping_cache AS SELECT company_id, domain_id, parent_company_id FROM pg.adtech.company_domain_mapping;
+CREATE TEMP TABLE company_domain_mapping_cache AS SELECT company_id, domain_id FROM pg.adtech.company_domain_mapping;
 
 CREATE TEMP TABLE store_app_store AS SELECT id, store, release_date FROM read_parquet($store_apps_key);
 
