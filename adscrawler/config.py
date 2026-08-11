c26f3c745f2bc421df13424ec0b6bfdbebf525aa
James O'Claire <james@oclaire.dev>
2026-08-11 12:45:01 +0800

    Use own log dir

--- adscrawler/config.py
+++ adscrawler/config.py
@@ -21,7 +21,7 @@ SSH_KNOWN_HOSTS = pathlib.Path(HOME, pathlib.Path(".ssh/known_hosts"))
 
 GEO_DATA_DIR = pathlib.Path(CONFIG_DIR, pathlib.Path("geo-data"))
 
-PROM_DIR = pathlib.Path("/var/lib/node_exporter/textfile_collector")
+PROM_DIR = pathlib.Path(LOG_DIR, pathlib.Path("/prom"))
 
 # Temp download dirs
 MITM_DIR = pathlib.Path(CONFIG_DIR, pathlib.Path("mitmlogs"))
@@ -104,6 +104,7 @@ def check_dirs() -> None:
         IPAS_INCOMING_DIR,
         SPLITS_INCOMING_DIR,
         GEO_DATA_DIR,
+        LOG_DIR,
         APP_ICONS_TMP_DIR,
     ]
     for _dir in dirs:
