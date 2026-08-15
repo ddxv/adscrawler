48837118b041c5dd262257a2facc125cccec03f7
James O'Claire <james@oclaire.dev>
2026-08-15 21:58:58 +0800

    Undo

--- adscrawler/dbcon/sql/query_apps_to_sdk_scan.sql
+++ adscrawler/dbcon/sql/query_apps_to_sdk_scan.sql
@@ -13,7 +13,6 @@ all_version_codes AS (
         version_codes AS vc
     INNER JOIN s3_file_keys AS sfk ON
         vc.id = sfk.version_code_id
-    WHERE sfk.myregion = 'loki'
 ),
 latest_version_codes AS (
     SELECT DISTINCT ON
